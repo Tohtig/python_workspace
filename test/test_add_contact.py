5073856309a690b4e5b11812eeaf784992922958
Ippolitov Anton <franktanwork@googlemail.com>
2016-10-11 20:33:27 +0300

    Выделен вспомогательный класс ContactHelper, тест контактов стал работать с фикстурой.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,79 +1,20 @@
 # -*- coding: utf-8 -*-
-import unittest
-from selenium.webdriver.firefox.webdriver import WebDriver
+import pytest
+from fixture.application import Application
 from model.contact import Contact
 
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-    
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def add_new_contact(self, contact):
-        wd = self.wd
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(contact.home_phone)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.adress)
-        wd.find_element_by_name("mobile").click()
-        wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(contact.mobile)
-        wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(contact.work_phone)
-        wd.find_element_by_name("fax").click()
-        wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(contact.fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(contact.byear)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def login(self):
-        wd = self.wd
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/index.php")
-
-    def test_test_add_contact(self):
-        wd = self.wd
-        self.open_home_page()
-        self.login()
-        self.add_new_contact(Contact(firstname="Ёжик", middlename="Ежов", lastname="Грибанов", nickname="Мачо", home_phone="zadarma.com", adress="Лес", mobile="нет розетки",
-                             work_phone="в лесу телефона нет", fax="пришлите с Печкиным", byear="1988"))
-        self.logout()
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_test_add_contact(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.create(Contact(firstname="Ёжик", middlename="Ежов", lastname="Грибанов", nickname="Мачо",
+                               home_phone="zadarma.com", adress="Лес", mobile="нет розетки",
+                               work_phone="в лесу телефона нет", fax="пришлите с Печкиным", byear="1988"))
+    app.session.logout()
