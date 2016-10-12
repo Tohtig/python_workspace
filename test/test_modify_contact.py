593dc2697ef39dacf5c8bed069aafd14b4a78381
Ippolitov Anton <franktanwork@googlemail.com>
2016-10-12 11:01:52 +0300

    Созданы тесты и впомогательные методы для удаления и модификации контактов

---
+++ test/test_modify_contact.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+
+
+def test_modify_first_contact(app):
+    app.open_home_page()
+    app.session.login(username="admin", password="secret")
+    app.contact.modify_first_contact(Contact(firstname="Modify_name", middlename="Modify_Ежов", lastname="Modify_Грибанов", nickname="Modify_Мачо",
+                               home_phone="Modify_zadarma.com", adress="Modify_Лес", mobile="Modify_нет розетки",
+                               work_phone="Modify_в лесу телефона нет", fax="Modify_пришлите с Печкиным", byear="1999"))
+    app.session.logout()
