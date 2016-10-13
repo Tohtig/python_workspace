6b01885fafaafffb016654124ba857d83808bff5
Ippolitov Anton <franktanwork@googlemail.com>
2016-10-13 13:42:56 +0300

    Декомпозиция повторяющихся методов группы
    Сокращает объем кода. Нужно повторить для контактов.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,17 @@
 from model.group import Group
 
 
-def test_del_first_group(app):
+def test_modify_first_group(app):
     app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="modify_group", header="modify_header", footer="modify_footer"))
+    app.session.logout()
+
+def test_modify_group_name(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(name="modify_group"))
+    app.session.logout()
+
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="modify_header"))
     app.session.logout()
